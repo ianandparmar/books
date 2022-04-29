627982bd16457099fc1081aa4f906f73d0ed383f
ianandparmar <sameeranandparmar@gmail.com>
2022-04-29 22:19:11 +0530

    entire

---
+++ Header2.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header=()=>{
+    return(
+        <div className="ui fixed menu">
+            <div className="ui container center">
+                <h2>Contact Manager </h2>
+                <h3>i am h3 for you</h3>
+                <h4>i AM H4 FOR YOU</h4>
+                <p>i am like to test git using git commands</p>
+            </div>
+        </div>
+    );
+}
+export default Header;
